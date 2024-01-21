20b11e244e6d34d0c1b3957dbd62d1be5017f4bb
signor1 <emmanuelomemgboji@gmail.com>
2024-01-21 22:34:18 +0100

    project setup

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,14 +1,14 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import Head from "next/head";
+import Image from "next/image";
 
-import Layout from '@components/Layout';
-import Section from '@components/Section';
-import Container from '@components/Container';
+import Layout from "@components/Layout";
+import Section from "@components/Section";
+import Container from "@components/Container";
 
 // Show data from https://www.tvmaze.com/api
-import shows from '@data/shows';
+import shows from "@data/shows";
 
-import styles from '../styles/Dashboard.module.scss';
+import styles from "../styles/Dashboard.module.scss";
 
 export default function Dashboard({ watched, tracking, favorites }) {
   return (
@@ -25,15 +25,15 @@ export default function Dashboard({ watched, tracking, favorites }) {
         <Container>
           <ul className={styles.stats}>
             <li>
-              <p className={styles.statsStat}>{ watched.length }</p>
+              <p className={styles.statsStat}>{watched.length}</p>
               <h3 className={styles.statsTitle}>Favorites</h3>
             </li>
             <li>
-              <p className={styles.statsStat}>{ tracking.length }</p>
+              <p className={styles.statsStat}>{tracking.length}</p>
               <h3 className={styles.statsTitle}>Tracking</h3>
             </li>
             <li>
-              <p className={styles.statsStat}>{ favorites.length }</p>
+              <p className={styles.statsStat}>{favorites.length}</p>
               <h3 className={styles.statsTitle}>Watched</h3>
             </li>
           </ul>
@@ -45,17 +45,24 @@ export default function Dashboard({ watched, tracking, favorites }) {
           <h2 className={styles.heading}>Favorites</h2>
 
           <ul className={styles.shows}>
-            {favorites.map(show => {
+            {favorites.map((show) => {
               return (
                 <li key={show.id}>
                   <a href={show.url} rel="noopener noreferrer">
-                    <Image width="300" height="422" src={show.image.medium} alt={`${show.name} Poster`} />
+                    <Image
+                      width="300"
+                      height="422"
+                      src={show.image.medium}
+                      alt={`${show.name} Poster`}
+                    />
                   </a>
                   <h3 className={styles.showsTitle}>
-                    <a href={show.url} rel="noopener noreferrer">{ show.name }</a>
+                    <a href={show.url} rel="noopener noreferrer">
+                      {show.name}
+                    </a>
                   </h3>
                 </li>
-              )
+              );
             })}
           </ul>
         </Container>
@@ -66,17 +73,24 @@ export default function Dashboard({ watched, tracking, favorites }) {
           <h2 className={styles.heading}>Tracking</h2>
 
           <ul className={styles.shows}>
-            {tracking.map(show => {
+            {tracking.map((show) => {
               return (
                 <li key={show.id}>
                   <a href={show.url} rel="noopener noreferrer">
-                    <Image width="300" height="422" src={show.image.medium} alt={`${show.name} Poster`} />
+                    <Image
+                      width="300"
+                      height="422"
+                      src={show.image.medium}
+                      alt={`${show.name} Poster`}
+                    />
                   </a>
                   <h3 className={styles.showsTitle}>
-                    <a href={show.url} rel="noopener noreferrer">{ show.name }</a>
+                    <a href={show.url} rel="noopener noreferrer">
+                      {show.name}
+                    </a>
                   </h3>
                 </li>
-              )
+              );
             })}
           </ul>
         </Container>
@@ -87,31 +101,38 @@ export default function Dashboard({ watched, tracking, favorites }) {
           <h2 className={styles.heading}>Watched</h2>
 
           <ul className={styles.shows}>
-            {watched.map(show => {
+            {watched.map((show) => {
               return (
                 <li key={show.id}>
                   <a href={show.url} rel="noopener noreferrer">
-                    <Image width="300" height="422" src={show.image.medium} alt={`${show.name} Poster`} />
+                    <Image
+                      width="300"
+                      height="422"
+                      src={show.image.medium}
+                      alt={`${show.name} Poster`}
+                    />
                   </a>
                   <h3 className={styles.showsTitle}>
-                    <a href={show.url} rel="noopener noreferrer">{ show.name }</a>
+                    <a href={show.url} rel="noopener noreferrer">
+                      {show.name}
+                    </a>
                   </h3>
                 </li>
-              )
+              );
             })}
           </ul>
         </Container>
       </Section>
     </Layout>
-  )
+  );
 }
 
 export async function getServerSideProps() {
   return {
     props: {
       watched: shows,
-      tracking: [...shows].sort(() => 0.5 - Math.random()).slice(0,6),
-      favorites: [...shows].sort(() => 0.5 - Math.random()).slice(0,3),
-    }
-  }
+      tracking: [...shows].sort(() => 0.5 - Math.random()).slice(0, 6),
+      favorites: [...shows].sort(() => 0.5 - Math.random()).slice(0, 3),
+    },
+  };
 }
