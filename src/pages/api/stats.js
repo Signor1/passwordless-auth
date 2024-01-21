d09d73cc907741ae8fa9ad4549e956c5975c640b
signor1 <emmanuelomemgboji@gmail.com>
2024-01-22 00:24:10 +0100

    feat: added a middleware on API routes

--- src/pages/api/stats.js
+++ src/pages/api/stats.js
@@ -1,6 +1,15 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { getAuth } from "@clerk/nextjs/server";
 
 export default function handler(req, res) {
+  const { userId } = getAuth(req);
+  console.log("userId", userId);
+
+  if (!userId) {
+    res.status(401).json({
+      status: "Unauthorized access",
+    });
+    return;
+  }
   res.status(200).json({
     favourites: 10,
     tracking: 5,
